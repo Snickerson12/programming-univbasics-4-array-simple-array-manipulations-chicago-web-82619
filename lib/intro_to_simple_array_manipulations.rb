056682eab9d2fd112cfcead5b92343c4b50c1b79
Laura Nickerson <laura.nickerson@gmail.com>
2019-08-09 22:04:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,3 +25,19 @@ end
 def using_concat(array1, array2)
   array1.concat(array2)
 end
+
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(String)
+end
+
+def using_delete_at(array, integer)
+  array.delete(integer)
+end
